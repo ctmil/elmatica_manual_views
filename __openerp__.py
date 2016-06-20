6da01e126b11a08dbc2e60ef52989bf41d7154a8
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-20 16:29:34 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Elmatica - Manual Views module',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['elmatica_sales_purchase'],
+    'data': [
+	'manual_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    # 'css': ['static/src/css/styles.css',],
+    'installable': True,
+}
