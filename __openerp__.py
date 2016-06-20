9aee1575108484fa60b6c8431795ecf4d28a8a91
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-20 18:06:24 -0300

    Temporary commit

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Elmatica - Manual Views module',
     'category': 'Sales',
     'version': '0.1',
-    'depends': ['elmatica_sales_purchase'],
+    'depends': ['elmatica_sales_purchase','elmatica_purchase_flow','purchase_requisition'],
     'data': [
 	'manual_view.xml',
     ],
