178229cc0390b9feda5e9ec50cd1a7470bbd42db
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-20 18:59:00 -0300

    Lp_headers_popup_so

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Elmatica - Manual Views module',
     'category': 'Sales',
     'version': '0.1',
-    'depends': ['elmatica_sales_purchase','elmatica_purchase_flow','purchase_requisition','product_lifecycle'],
+    'depends': ['elmatica_sales_purchase','elmatica_purchase_flow','purchase_requisition','product_lifecycle','sale_stock'],
     'data': [
 	'manual_view.xml',
     ],
